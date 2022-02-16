0a8f8b1fd5c6d38f10ec3b509909fa3a3d12d0d4
Aaditya Srivastava <47076498+Aadvi@users.noreply.github.com>
2022-02-16 12:17:24 +0530

    Add files via upload

---
+++ aadicodex/factorial.sh
@@ -0,0 +1,12 @@
+#FACTORIAL PROGRAM
+echo "Enter a Number"
+read num
+fact=1 
+
+while [ $num -gt 1 ]
+do
+ fact=$((fact * num)) #fact = fact*num
+ num=$((num - 1)) #num = num-1
+done
+ 
+echo $fact
